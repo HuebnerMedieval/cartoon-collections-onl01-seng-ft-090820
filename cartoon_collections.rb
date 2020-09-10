968551b21e566053b088ee77fe5510a6ccc0721c
Eli Huebner <elimhuebner@gmail.com>
2020-09-10 13:16:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def summon_captain_planet(planateer_calls)
 end
 
 def long_planeteer_calls(array_of_calls)
-  array_of_calls.
+  array_of_calls.include?()
 end
 
 def find_the_cheese# code an argument here
